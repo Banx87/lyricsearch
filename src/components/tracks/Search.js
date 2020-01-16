59648d3fecdd6e0cc6f2f5a462231ada50374bd1
Daemon87 <joni.lindholm@gmail.com>
2020-01-16 16:14:15 +0200

    Update Search.js

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -39,9 +39,7 @@ class Search extends Component {
       <Consumer>
         {value => {
           const { dispatch } = value;
-          {/* {
-            /* console.log(value); */
-          } */}
+
           return (
             <div className="card card-body mb-4 p-4">
               <h1 className="display-4 text-center">
